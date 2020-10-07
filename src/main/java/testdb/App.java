132c9c1c591881b7655f525ffed058e48ec0fd62
nicrieser <nico.rieser@gmail.commkdir hello-worldcd hello-worldgit initgit statusgit config --global user.email nico.rieser@gmail.com>
2020-10-07 16:06:54 +0200

    Erste Methode fertig

--- src/main/java/testdb/App.java
+++ src/main/java/testdb/App.java
@@ -1,10 +1,7 @@
 package testdb;
 
 import java.io.PrintStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 
 /**
@@ -17,7 +14,7 @@ public class App
     {
         System.out.println("Test");
 
-        try{
+        /*try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test","root","");
             Statement stmt=con.createStatement();
@@ -26,16 +23,64 @@ public class App
             while(rs.next())
                 System.out.println(rs.getInt(1)+"  "+rs.getString(3));
             con.close();
-        }catch(Exception e){ System.out.println(e);}
+        }
+        catch(Exception e){ System.out.println(e);}
+
+         */
+        showInvoice();
+    }
+
+    static Connection con;
+    static Statement stmt;
+    public static void connectDatabase()
+    {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
+            stmt=con.createStatement();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public static void showInvoice()
+    {
+        connectDatabase();
+
+
+
+        try
+        {
+            ResultSet rs=stmt.executeQuery("select * from invoice");
+            while(rs.next())
+            {
+
+                System.out.println(rs.getInt(1) + "  " +rs.getDate(2) + " " + rs.getString(3) +  " " +
+                                    rs.getInt(4) + " " + rs.getInt(5) );
+
+            }
+            con.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+
     }
 
 
-    public static void showInvoices()
+    public static void insertInvoice(Date date, String description, double value, Boolean paid)
     {
 
+    }
+    public static void updateInvoice(int id, Date date, String description, double value, Boolean paid)
+    {
 
     }
-    public static void insertInvoices()
+    public static void deleteInvoice(int id)
     {
 
     }
