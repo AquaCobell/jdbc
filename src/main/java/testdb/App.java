d1edef682aa51c5cea356ba4037cbaa105160306
nicrieser <nico.rieser@gmail.commkdir hello-worldcd hello-worldgit initgit statusgit config --global user.email nico.rieser@gmail.com>
2020-10-07 15:52:40 +0200

    Datenbank Verbindung hergestellt. 2 Methodenköpfe erstellt

--- src/main/java/testdb/App.java
+++ src/main/java/testdb/App.java
@@ -16,16 +16,29 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Test");
-        //PrintStream stream = new PrintStream(System.out);
+
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/jdpc","root","");
+            Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test","root","");
             Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("select Age,Vorname from namen");
-            //stream.println(rs);
+            ResultSet rs=stmt.executeQuery("select * from invoice");
+
             while(rs.next())
-                System.out.println(rs.getInt("Age")+"  "+rs.getString("Vorname"));
+                System.out.println(rs.getInt(1)+"  "+rs.getString(3));
             con.close();
         }catch(Exception e){ System.out.println(e);}
     }
+
+
+    public static void showInvoices()
+    {
+
+
+    }
+    public static void insertInvoices()
+    {
+
+    }
+
+
 }
