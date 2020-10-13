7bfc8c7f52904427bd67e23a83dc2e1546c92b64
nicrieser <nico.rieser@gmail.commkdir hello-worldcd hello-worldgit initgit statusgit config --global user.email nico.rieser@gmail.com>
2020-10-13 16:56:39 +0200

    InvoiceDAOIMPL.java erstellt

---
+++ src/main/java/testdb/InvoiceDAOImpl.java
@@ -0,0 +1,35 @@
+package testdb;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InvoiceDAOImpl implements InvoiceDAO
+{
+
+
+
+    @Override
+    public List showInvoice()
+    {
+        return null;
+    }
+
+    @Override
+    public void updateInvoice(int id, Date date, String description, double value, byte paid)
+    {
+
+    }
+
+    @Override
+    public void deleteInvoice(int id)
+    {
+
+    }
+
+    @Override
+    public void insertInvoice(Date datum, String besch, int wert, byte bez)
+    {
+
+    }
+}
