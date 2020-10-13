9352579605dc3bdcdd6b1e825b31d8f4f36796a2
nicrieser <nico.rieser@gmail.commkdir hello-worldcd hello-worldgit initgit statusgit config --global user.email nico.rieser@gmail.com>
2020-10-13 19:23:46 +0200

    InvoiceDAOImpl ausgetestet.

--- src/main/java/testdb/App.java
+++ src/main/java/testdb/App.java
@@ -17,19 +17,7 @@ public class App
         System.out.println("Test");
 
 
-        /*try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test","root","");
-            Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("select * from invoice");
 
-            while(rs.next())
-                System.out.println(rs.getInt(1)+"  "+rs.getString(3));
-            con.close();
-        }
-        catch(Exception e){ System.out.println(e);}
-
-         */
 
         //Date datum = new java.sql.Date(Calendar.getInstance().getTime().getTime());;
         //Date datum = new Date(System.currentTimeMillis());
